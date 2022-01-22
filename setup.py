ad0fd834880f5c90b6df8e544f8190885d047089
Pragyan Shrestha <pragyanOne@gmail.com>
2022-01-23 00:37:24 +0545

    Added JV Support

--- setup.py
+++ setup.py
@@ -13,7 +13,8 @@ setup(
   download_url = 'https://github.com/pragyanone/pytender/archive/refs/tags/1.5.tar.gz',
   keywords = ['ppmo', 'bid', 'tender',],   # Keywords that define your package best
   install_requires=[
-          'python-docx',
+          'python-docx==0.8.11',
+          'num2words==0.5.10'
       ],
   classifiers=[
     'Development Status :: 5 - Production/Stable',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
