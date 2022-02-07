02d1cbb832e0efd537e059b4662acdcc28256b3f
Pragyan Shrestha <pragyanOne@gmail.com>
2022-02-07 18:27:02 +0545

    speedup, provision for future dates

--- pytender/adbs/adbs.py
+++ pytender/adbs/adbs.py
@@ -1,4 +1,10 @@
-# 2000-2090
+# For future dates:
+# 1. Change start/end values accordingly.
+# 2. Add new rows of bs_data. Dont forget to add ',' to the existing last row.
+
+bsStart, bsEnd = "2000-1-1", "2090-12-30"
+adStart, adEnd = "1943-4-14", "2034-4-13"
+
 bs_data = """
 30,32,31,32,31,30,30,30,29,30,29,31,
 31,31,32,31,31,31,30,29,30,29,30,30,
@@ -99,8 +105,8 @@ ad_data = (31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
 def unpackDate(date):
     try:
         y, m, d = [int(item) for item in date.split("-")]
-    except ValueError:
-        return "Enter date in proper format: y-m-d"
+    except:
+        raise AssertionError("Enter date in proper format: y-m-d")
     assert 0 < m < 13, "Invalid date. Check month."
     return (y, m, d)
 
@@ -112,13 +118,13 @@ def isLeapYear(year):
 def ad2days(ADdate):
     y, m, d = unpackDate(ADdate)
     assert (
-        1943 < y < 2034
-        or (y == 1943 and m >= 4 and d >= 14)
-        or (y == 2034 and m <= 4 and d <= 13)
-    ), "Invalid date. Supported dates: 1943-4-14 to 2034-4-13"
-    assert (
-        isLeapYear(y) and m == 2 and 0 < d <= bs_data[(y - 2000) * 12 + m - 1] + 1
-    ) or (0 < d <= ad_data[m - 1]), "Invalid date. Check day."
+        adStart["y"] < y < adEnd["y"]
+        or (y == adStart["y"] and m >= adStart["m"] and d >= adStart["d"])
+        or (y == adEnd["y"] and m <= adEnd["m"] and d <= adEnd["d"])
+    ), f"Invalid date. Supported dates: {adStartS} to {adEndS}"
+    assert (isLeapYear(y) and m == 2 and 0 < d <= ad_data[m - 1] + 1) or (
+        0 < d <= ad_data[m - 1]
+    ), "Invalid date. Check day."
     days = 0
     y -= 1
     days += int(y * 365 + y / 4 - y / 100 + y / 400)
@@ -131,17 +137,20 @@ def ad2days(ADdate):
 
 def bs2days(BSdate):
     y, m, d = unpackDate(BSdate)
-    assert 1999 < y < 2091, "Invalid date. Supported dates: 2000-1-1 to 2090-12-30."
-    assert 0 < d <= bs_data[(y - 2000) * 12 + m - 1], "Invalid date. Check day."
-    days = sum(bs_data[0 : (y - 2000) * 12])
-    days += sum(bs_data[(y - 2000) * 12 : (y - 2000) * 12 + m - 1])
+    assert (
+        bsStart["y"] <= y <= bsEnd["y"]
+    ), f"Invalid date. Supported dates: {bsStartS} to {bsEndS}."
+    assert 0 < d <= bs_data[(y - bsStart["y"]) * 12 + m - 1], "Invalid date. Check day."
+    days = sum(bs_data[0 : (y - bsStart["y"]) * 12])
+    days += sum(bs_data[(y - bsStart["y"]) * 12 : (y - bsStart["y"]) * 12 + m - 1])
     days += d
     return days
 
 
 def days2ad(days):
-    y = 1
-    m = 0
+    y = adStart["y"]
+    m = adStart["m"] - 1
+    days -= ad2days(adStartS) - adStart["d"]
     while days > 0:
         d = days
         if m == 12:
@@ -157,7 +166,7 @@ def days2ad(days):
 
 
 def days2bs(days):
-    y = 2000
+    y = bsStart["y"]
     m = 0
     while days > 0:
         d = days
@@ -170,22 +179,31 @@ def days2bs(days):
 
 
 def ad2bs(date):
-    return days2bs(ad2days(date) - ad2days("1943-4-14") + 1)
+    return days2bs(ad2days(date) - ad2days(adStartS) + 1)
 
 
 def bs2ad(date):
-    return days2ad(bs2days(date) + ad2days("1943-4-14") - 1)
+    return days2ad(bs2days(date) + ad2days(adStartS) - 1)
 
+
+def makeDict(*dates):
+    keys = ("y", "m", "d")
+    for date in dates:
+        yield {key: value for key, value in zip(keys, unpackDate(date))}
+
+
+bsStartS, bsEndS, adStartS, adEndS = bsStart, bsEnd, adStart, adEnd
+bsStart, bsEnd, adStart, adEnd = makeDict(bsStart, bsEnd, adStart, adEnd)
 
 if __name__ == "__main__":
     while True:
         choice = input("1. AD2BS\n2. BS2AD\nq. Quit\nChoice? ")
         if choice == "q":
             break
+        elif not choice == "1" or choice == "2":
+            print("Invalid Choice")
         date = input("Date: ")
         if choice == "1":
             print(f"BS Date: {ad2bs(date)}")
         elif choice == "2":
             print(f"AD Date: {bs2ad(date)}")
-        else:
-            print("Invalid Choice")
